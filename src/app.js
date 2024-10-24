4c5d420e28a5433cb84dbcb1b9675d2141bb9d24
Jaramillo Vasquez <ejaramillo@sanimax.com.co>
2024-10-24 15:03:27 -0500

    Implementing Pipeline

--- src/app.js
+++ src/app.js
@@ -11,25 +11,22 @@ app.use(bodyParser.json());
 // Routes
 app.use('/api/auth', userRoutes);
 
+// MongoDB Connection (solo si no está en modo test)
 if (process.env.NODE_ENV !== 'test') {
   mongoose.connect(process.env.MONGO_URL || 'mongodb://root:example@mongodb:27017/auth-service?authSource=admin', {
-   
+    // Ya no es necesario usar estos parámetros con la versión 4.0.0 del driver
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
   })
     .then(() => console.log('Connected to MongoDB for Auth Service'))
-    .catch((err) => console.error(err));
+    .catch((err) => console.error('MongoDB connection error:', err));
 }
-// MongoDB Connection
-//mongoose.connect('mongodb://root:example@mongodb:27017/auth-service?authSource=admin', {
-  //mongodb://root:example@mongodb:27017/auth-service?authSource=admin
-  // useNewUrlParser: true,
-  // useUnifiedTopology: true,
-  //mongodb://localhost:27017/auth-service
-//})
-//  .then(() => console.log('Connected to MongoDB for Auth Service'))
-//  .catch((err) => console.error(err));
 
-const PORT = process.env.PORT || 3005;
-app.listen(PORT, () => console.log(`Auth Service running on port ${PORT}`));
+// Start server only if not in test environment
+if (process.env.NODE_ENV !== 'test') {
+  const PORT = process.env.PORT || 3005;
+  app.listen(PORT, () => console.log(`Auth Service running on port ${PORT}`));
+}
 
 module.exports = app;
 
